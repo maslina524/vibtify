b23d2ccb95dd4728b62f39f58f96a36d6a00b4a7
maslina524 <luk.kirill2804@gmail.com>
2026-05-22 19:47:19 +0300

    added raindow_lightbar_test

--- src/lib.rs
+++ src/lib.rs
@@ -92,4 +92,40 @@ mod tests {
             thread::sleep(time::Duration::from_millis(10));
         }
     }
+
+    fn hsv_to_rgb(h: f64, s: f64, v: f64) -> (u8, u8, u8) {
+        let h = h % 360.0;
+        
+        let c = v * s; // Chroma
+        let x = c * (1.0 - ((h / 60.0) % 2.0 - 1.0).abs());
+        let m = v - c;
+        
+        let (r1, g1, b1) = match h {
+            0.0..=60.0 => (c, x, 0.0),
+            60.0..=120.0 => (x, c, 0.0),
+            120.0..=180.0 => (0.0, c, x),
+            180.0..=240.0 => (0.0, x, c),
+            240.0..=300.0 => (x, 0.0, c),
+            _ => (c, 0.0, x),
+        };
+        
+        let r = ((r1 + m) * 255.0).round() as u8;
+        let g = ((g1 + m) * 255.0).round() as u8;
+        let b = ((b1 + m) * 255.0).round() as u8;
+        
+        (r, g, b)
+    }
+
+    #[test]
+    fn rainbow_lightbar_test() {
+        let gamepad = &get_gamepads().unwrap()[0];
+        for x in 0..1000 {
+            let (r, g, b) = hsv_to_rgb((x % 360) as f64, 1., 1.);
+            println!("{r}-{g}-{b}");
+            if let Err(e) = gamepad.set_lightbar(r, g, b) {
+                panic!("{e:?}");
+            };
+            thread::sleep(time::Duration::from_millis(10));
+        }
+    }
 }
